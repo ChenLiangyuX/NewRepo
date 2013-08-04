a27c0eedd29d2fc54390e678c5050079d1741afa
ChenLiangyuX <chenliangyu@baixing.net>
2013-08-04 17:26:25 +0800

    3

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 /**
  * Created by JetBrains PhpStorm.
- * User: Administrator
+ * User: ChenLiangyuX
  * Date: 13-8-4
  * Time: 下午3:28
  * To change this template use File | Settings | File Templates.
  */
-	echo "yes";
+echo "hehe";
+echo "yes";
+echo "No";
+echo "kkk";
+echo "hehe";
+
+$group = array('c' , 'l' , 'y');
+
+foreach ($group as $index => $eachGroup) {
+	echo "{$index} => {$eachGroup}";
+}
