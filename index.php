9516707acec7e1aa1259c7715ce78eaf93239d96
ChenLiangyuX <chenliangyu@baixing.net>
2013-08-04 15:32:51 +0800

    Committ index
    呵呵

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 13-8-4
+ * Time: 下午3:28
+ * To change this template use File | Settings | File Templates.
+ */
+	echo "yes";
